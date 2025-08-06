6172f62ca0bb11a60a435f9150c628f9aa4efe42
vrindadixit17 <168673148+vrindadixit17@users.noreply.github.com>
2025-08-06 20:48:37 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import SearchBox from './SearchBox'
+import InfoBox from './InfoBox'
+import WeatherApp from './WeatherApp'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='WeatherApp'>
+
+      <WeatherApp />
+    </div>
+  )
+}
+
+export default App
